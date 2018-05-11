3a4be49220e11d67832fdcb66abcd556201f1371
JoshuaDDominguez <32340127+JoshuaDDominguez@users.noreply.github.com>
2018-05-11 10:22:03 +0200

    Datos pedidos

--- src/org/myorg/empaquetado/EmpaquetadoAutomatico.java
+++ src/org/myorg/empaquetado/EmpaquetadoAutomatico.java
@@ -7,6 +7,8 @@ package org.myorg.empaquetado;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+import javax.swing.JOptionPane;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
@@ -27,5 +29,17 @@ public final class EmpaquetadoAutomatico implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO implement action body
+       
+        // Creamos un objeto para acceder a los datos
+        Datos datos = new Datos();
+
+        //Pedimos todos los datos necesarios para el comando
+        datos.setvBcategory(JOptionPane.showInputDialog("Categoria (Bcategory)"));
+        datos.setvOutdir(JOptionPane.showInputDialog("Direccion donde guardar el ejecutable (Outdir)"));
+        datos.setvOutfiles(JOptionPane.showInputDialog("Nombre del archivo (Outfile)"));
+        datos.setVsrcdir(JOptionPane.showInputDialog("Direccion de las src (srcdir)"));
+        datos.setVappclass(JOptionPane.showInputDialog("Direccion de .jar (srcfiles)"));
+        datos.setvName(JOptionPane.showInputDialog("Nombre de la aplicacion (Name)"));
+        datos.setVtitle(datos.getvName());
     }
 }
