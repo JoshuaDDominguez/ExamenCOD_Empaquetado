91645b483ea1d36ec38b3b643dfa5a17fab99c19
JoshuaDDominguez <32340127+JoshuaDDominguez@users.noreply.github.com>
2018-05-11 10:10:02 +0200

    Gets y Sets añadidos, asi como el toString

--- src/org/myorg/empaquetado/Datos.java
+++ src/org/myorg/empaquetado/Datos.java
@@ -7,17 +7,18 @@ package org.myorg.empaquetado;
 
 /**
  * Clase Objeto para guardar los datos del programa a empaquetar.
+ *
  * @author Joshua
  */
 public class Datos {
 
-    String extension, vOutdir, vOutfiles, Vsrcfiles, Vappclass, vName, Vtitle;
+    String vBcategory, vOutdir, vOutfiles, Vsrcfiles, Vappclass, vName, Vtitle;
 
     public Datos() {
     }
 
-    public Datos(String extension, String vOutdir, String vOutfiles, String Vsrcfiles, String Vappclass, String vName, String Vtitle) {
-        this.extension = extension;
+    public Datos(String vBcategory, String vOutdir, String vOutfiles, String Vsrcfiles, String Vappclass, String vName, String Vtitle) {
+        this.vBcategory = vBcategory;
         this.vOutdir = vOutdir;
         this.vOutfiles = vOutfiles;
         this.Vsrcfiles = Vsrcfiles;
@@ -26,9 +27,65 @@ public class Datos {
         this.Vtitle = Vtitle;
     }
 
+    public String getvBcategory() {
+        return vBcategory;
+    }
+
+    public String getvOutdir() {
+        return vOutdir;
+    }
+
+    public String getvOutfiles() {
+        return vOutfiles;
+    }
+
+    public String getVsrcfiles() {
+        return Vsrcfiles;
+    }
+
+    public String getVappclass() {
+        return Vappclass;
+    }
+
+    public String getvName() {
+        return vName;
+    }
+
+    public String getVtitle() {
+        return Vtitle;
+    }
+
+    public void setvBcategory(String vBcategory) {
+        this.vBcategory = vBcategory;
+    }
+
+    public void setvOutdir(String vOutdir) {
+        this.vOutdir = vOutdir;
+    }
+
+    public void setvOutfiles(String vOutfiles) {
+        this.vOutfiles = vOutfiles;
+    }
+
+    public void setVsrcfiles(String Vsrcfiles) {
+        this.Vsrcfiles = Vsrcfiles;
+    }
+
+    public void setVappclass(String Vappclass) {
+        this.Vappclass = Vappclass;
+    }
+
+    public void setvName(String vName) {
+        this.vName = vName;
+    }
+
+    public void setVtitle(String Vtitle) {
+        this.Vtitle = Vtitle;
+    }
+
     @Override
     public String toString() {
-        return "Datos{" + "extension=" + extension + ", vOutdir=" + vOutdir + ", vOutfiles=" + vOutfiles + ", Vsrcfiles=" + Vsrcfiles + ", Vappclass=" + Vappclass + ", vName=" + vName + ", Vtitle=" + Vtitle + '}';
+        return "Datos{" + "vBcategory=" + vBcategory + ", vOutdir=" + vOutdir + ", vOutfiles=" + vOutfiles + ", Vsrcfiles=" + Vsrcfiles + ", Vappclass=" + Vappclass + ", vName=" + vName + ", Vtitle=" + Vtitle + '}';
     }
 
 }
