ecf1a4dad2511bb3ceb588b251513263d3f55487
JoshuaDDominguez <32340127+JoshuaDDominguez@users.noreply.github.com>
2018-05-11 09:49:42 +0200

    Commint inicial

---
+++ src/org/myorg/empaquetado/Datos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.myorg.empaquetado;
+
+/**
+ *
+ * @author Joshua
+ */
+public class Datos {
+
+    String extension, vOutdir, vOutfiles, Vsrcfiles, Vappclass, vName, Vtitle;
+
+    public Datos() {
+    }
+
+    public Datos(String extension, String vOutdir, String vOutfiles, String Vsrcfiles, String Vappclass, String vName, String Vtitle) {
+        this.extension = extension;
+        this.vOutdir = vOutdir;
+        this.vOutfiles = vOutfiles;
+        this.Vsrcfiles = Vsrcfiles;
+        this.Vappclass = Vappclass;
+        this.vName = vName;
+        this.Vtitle = Vtitle;
+    }
+
+    @Override
+    public String toString() {
+        return "Datos{" + "extension=" + extension + ", vOutdir=" + vOutdir + ", vOutfiles=" + vOutfiles + ", Vsrcfiles=" + Vsrcfiles + ", Vappclass=" + Vappclass + ", vName=" + vName + ", Vtitle=" + Vtitle + '}';
+    }
+
+}
